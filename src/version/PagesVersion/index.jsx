1e42f49e83f0bc3eaf5bc951eab1ecf4bced0c80
chuck002 <ljavy86@gmail.com>
2024-04-12 15:42:53 -0300

    Try navbar

---
+++ src/version/PagesVersion/index.jsx
@@ -0,0 +1,12 @@
+import PageLandingNavBar from '../LandingVersion/components/PageLandingNavBar';
+
+function PageVersion(){
+
+    return (
+        <div>
+            <PageLandingNavBar />
+        </div>
+    );
+}
+
+export default PageVersion;
