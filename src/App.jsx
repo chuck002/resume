649990207c5b41d68029d40f7a7a79b529823ccb
chuck002 <ljavy86@gmail.com>
2024-04-12 15:06:17 -0300

    Add react Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
 import GridLanguages from './components/GridLanguages'
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <div>
-        <GridLanguages />
-        
-      </div>
+        <Routes>
+          <Route path="/" element={<div><h1>HomePage</h1></div>} />
+          <Route path="/Experience" element={<div><h1>Expericence Page</h1></div>} />
+          <Route path="/Education" element={<div><h1>Education Page</h1></div>} />
+          <Route path="/Skills" element={<div><h1>Skills Page</h1></div>} />
+          <Route path="/Languages" element={<GridLanguages />} />
+        </Routes>
     </>
   )
 }
