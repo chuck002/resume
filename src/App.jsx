0142ea072f52bad42059d56f2adc4f3a0a35b4d8
chuck002 <ljavy86@gmail.com>
2024-04-18 14:54:56 -0300

    Add projects

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import PersonalData from './version/PagesVersion/Pages/PersonalData/index.jsx'
 import Experience from './version/PagesVersion/Pages/Experience/index.jsx'
 import Education from './version/PagesVersion/Pages/Education/index.jsx'
 import Skills from './version/PagesVersion/Pages/Skills/index.jsx'
+import Projects from './version/PagesVersion/Pages/Projects/index.jsx'
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/Experience" element={<Experience />} />
           <Route path="/Education" element={<Education />} />
           <Route path="/Skills" element={<Skills />} />
+          <Route path="*" element={<Projects />} />
         </Routes>
     </>
   )
--- src/components/PageNavBar/index.jsx
+++ src/components/PageNavBar/index.jsx
@@ -12,12 +12,13 @@ function PageNavBar() {
                 <div className="NavBar-Container-Menu">
                     <ul>
                         <li><Link to="/">Home</Link></li>
-                        <li><Link to="/Experience">Experience</Link></li>
-                        <li><Link to="/Education">Education</Link></li>
-                        <li><Link to="/Skills">Skills</Link></li>
+                        <li><Link to="/Experience">Experiencia</Link></li>
+                        <li><Link to="/Education">Educacion</Link></li>
+                        <li><Link to="/Skills">Habilidades</Link></li>
+                        <li><Link to="/Projects">Proyectos</Link></li>
                         <li><Link to="/PersonalData">Contacto</Link></li>
-                        <li><Link to="/PersonalData">B</Link></li>
-                        <li><Link to="/PersonalData">T</Link></li>
+                        <li><Link to="/PersonalData">TIPO</Link></li>
+                        <li><Link to="/PersonalData">LENGUA</Link></li>
                     </ul>
                 </div>
             </div>
