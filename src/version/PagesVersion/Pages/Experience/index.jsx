870529fc54658d079ead0a2acbfbebd54f519833
chuck002 <ljavy86@gmail.com>
2024-04-22 00:12:47 -0300

    Optimize data experience

--- src/version/PagesVersion/Pages/Experience/index.jsx
+++ src/version/PagesVersion/Pages/Experience/index.jsx
@@ -1,8 +1,10 @@
 import PageNavBar from '../../../../components/PageNavBar';
 import PageFooter from '../../../../components/PageFooter';
+import ExperienceJson from '../../../../../src/assets/Data/Experience.json';
 import './Experience.css'
 
 function Experience() {
+  console.log(ExperienceJson);
   return (
     <>
       <PageNavBar />
@@ -13,57 +15,22 @@ function Experience() {
           </div>
           <div className='Container-Card-Content'>
             <ul>
-              <li>
-                <div className='Lista-Experience-Title'>
-                  <span className='Experience-fecha'>02/2022- Actualidad</span>
-                  <span className='Experience-empresa'>Solucredito SA (Republica Dominicana)</span>
-                </div>
-                <div className='Lista-Experience-Content'>
-                  <p>Actualmente trabajo como desarrollador FullStack, en
-                    su plataforma de venta de servicios financieros en Java
-                    8 bajo el framework SpringBoot con Maven, una Base
-                    Relacional SQL y con un frontEnd desarrollado de
-                    manera mixta entre Thymeleaf y JS con AJAX.</p>
-                </div>
-              </li>
-              <li>
-                <div className='Lista-Experience-Title'>
-                  <span className='Experience-fecha'>01/2022- Actualidad</span>
-                  <span className='Experience-empresa'>Pulpou SA (Argentina)</span>
-                </div>
-                <div className='Lista-Experience-Content'>
-                  <p>Trabajo Actualmente de manera remota en la
-                    plataforma de scrapping para Brand Protection,
-                    generando nuevas features y resolviendo issues en la
-                    nueva libreria con JS en Node.</p>
-                </div>
-              </li>
-              <li>
-                <div className='Lista-Experience-Title'>
-                  <span className='Experience-fecha'>07/2021 - 01/2022</span>
-                  <span className='Experience-empresa'>Pulpou SA (Argentina)</span>
-                </div>
-                <div className='Lista-Experience-Content'>
-                  <p>Trabaje en el desarrollo de nuevas features y resolucion
-                    de issues en su plataforma REST de Brand Protection,
-                    que se encontraba compuesta de un frontEnd en
-                    ReactJS y con un Backend diseñada en Python con Flask
-                    y SQLAlchemy.</p>
-                </div>
-              </li>
-              <li>
-                <div className='Lista-Experience-Title'>
-                  <span className='Experience-fecha'>2010 - 2019</span>
-                  <span className='Experience-empresa'>Ford Argentina SA</span>
-                </div>
-                <div className='Lista-Experience-Content'>
-                  <p>Me desempeñe durante 10 años como
-                    operario en planta, trabajando en grupo
-                    primero en la linea de producción.
-                    Y luego desarrollando tareas varias, como
-                    Data Entry de piezas fallidas y materiales.</p>
-                </div>
-              </li>
+                {ExperienceJson.languaje.spanish.Experience.map((item, index) => {
+                  return (
+                    <li key={index}>
+                      <div>
+                        <div><span className='Experience-empresa'>{item.Company}</span></div>
+                        <div className='Lista-Experience-Title'>
+                          <span className='Experience-fecha'>{item.Duration}</span>
+                        </div>
+                        <div className='Lista-Experience-Content'>
+                          <p>{item.Description}</p>
+                        </div>
+                      </div>
+                    </li>
+                  );
+
+                }, [])}
             </ul>
           </div>
 
