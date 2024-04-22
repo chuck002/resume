9aa4b684a3ad34786a7c7fd0febfd5fcdedf149b
chuck002 <ljavy86@gmail.com>
2024-04-21 23:02:59 -0300

    Add Skills to skill list

--- src/version/PagesVersion/Pages/Skills/index.jsx
+++ src/version/PagesVersion/Pages/Skills/index.jsx
@@ -12,24 +12,60 @@ function Education() {
             <h3>Conocimientos</h3>
           </div>
           <div className='Container-Card-Content'>
-            <p>Lenguajes de programación.
-Java, CSS (Framework Bootstrap), HTML5, c,
-Javascript con Node y ReactJS y Python con Flask,
-etc.
-Herramientas de Desarrollo.
-Manejo sistemas Linux y Windows, CLI bash, SQL,
-AWS S3, ORM (Con Hibernate en SpringBoot,
-SQLAlchemy en Flask y Sequelize en NodeJS).
-Instalacion y Administracion Basica de Servidores
-Apache
-(phpMyAdmin) y MySQL, para pruebas en
-localhost.
-Conocimiento de sistemas de control de versiones
-con Git y manejo de repositorios con Github.
-Manejo basico de contenedores, con Docker.
-Utilizo herramientas de metodologias Agiles tales
-como monday, Jira y Trello.
-Español Nativo. Ingles nivel Intermedio.</p>
+            <p><h1>Lenguajes de programación.</h1>
+            <ul>
+              <li>Java</li>
+              <li>JavaScript</li>
+              <li>SQL</li>
+              <li>HTML5</li>
+              <li>CSS</li>
+            </ul>
+            <h1>Frameworks y Librerias.</h1>
+            <ul>
+              <li>SpringBoot</li>
+              <li>Hibernate</li>
+              <li>NodeJS</li>
+              <li>Express</li>
+              <li>ReactJS</li>
+              <li>Bootstrap</li>
+              <li>Sequelize</li>
+              <li>Material UI</li>
+              <li>Selenium</li>
+              <li>Puppeteer</li>
+            </ul>
+            <h1>Base De Datos</h1>
+            <ul>
+            <li><h2>Relacionales</h2></li>
+            <ul>
+              <li>MySQL</li>
+              <li>SQLite</li>
+            </ul>
+            <li><h2>No Relacionales</h2></li>
+            <ul>
+              <li>MongoDB</li>
+            </ul>
+            </ul>
+            <h1>Herramientas de Desarrollo</h1>
+            <ul>
+              <li>Git</li>
+              <li>Github</li>
+              <li>Postman</li>
+              <li>Visual Studio Code</li>
+              <li>Eclipse</li>
+              <li>AWS S3</li>
+              <li>Docker</li>
+            </ul>
+            <h1>Sistemas Operativos</h1>
+            <ul>
+              <li>Windows</li>
+              <li>Linux</li>
+            </ul>
+            <h1>Herramientas de Gestion de Tickets</h1>
+            <ul>
+              <li>Monday</li>
+              <li>Jira</li>
+              <li>Trello</li>
+            </ul></p>
           </div>
 
         </div>
