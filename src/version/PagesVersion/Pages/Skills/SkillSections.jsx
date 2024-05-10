e18dab5033285b0fd54025771615c27172955c1b
chuck002 <ljavy86@gmail.com>
2024-05-10 07:57:25 -0300

    Skills Added

---
+++ src/version/PagesVersion/Pages/Skills/SkillSections.jsx
@@ -0,0 +1,36 @@
+import Skills from '../../../../assets/Data/Skills.json'
+import { Card, CardContent, Typography, Grid, Tooltip } from '@mui/material';
+
+const SkillsSection = () => {
+    return (
+        <div>
+            <Typography variant="h4" gutterBottom>
+                Mis Habilidades
+            </Typography>
+            <Grid container spacing={3}>
+                {Skills.languaje.spanish.Habilidades.map((item, index) => {
+                    return (
+                        <Grid item xs={12} sm={6} md={4} key={index}>
+                            <Card>
+                                <CardContent>
+                                    <Typography variant="h6">{item.title}</Typography>
+                                    <Typography variant="body2">
+                                        {Array.from(item.data).map((item2, index) => {
+                                            return (
+                                                <Tooltip key={index} title={item2.description} arrow>
+                                                <Typography component="span">- {item2.name} <br /></Typography>
+                                                </Tooltip>
+                                            );
+                                        })}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    );
+                }, [])}
+            </Grid>
+        </div>
+    );
+};
+
+export default SkillsSection;
--- src/version/PagesVersion/Pages/Skills/index.jsx
+++ src/version/PagesVersion/Pages/Skills/index.jsx
@@ -1,71 +1,15 @@
 import PageNavBar from '../../../../components/PageNavBar';
 import PageFooter from '../../../../components/PageFooter';
 import './Skills.css'
+import SkillsSection from './SkillSections';
+
 
 function Education() {
   return (
     <>
       <PageNavBar />
         <div className='Container-Card-Skills'>
-          <div className='Container-Card-Title-Skills'>
-            <h3>Conocimientos</h3>
-          </div>
-          <div className='Container-Card-Content-Skills'>
-            <h1>Lenguajes de programación.</h1>
-            <p><ul>
-              <li>Java</li>
-              <li>JavaScript</li>
-              <li>SQL</li>
-              <li>HTML5</li>
-              <li>CSS</li>
-            </ul>
-            <h1>Frameworks y Librerias.</h1>
-            <ul>
-              <li>SpringBoot</li>
-              <li>Hibernate</li>
-              <li>NodeJS</li>
-              <li>Express</li>
-              <li>ReactJS</li>
-              <li>Bootstrap</li>
-              <li>Sequelize</li>
-              <li>Material UI</li>
-              <li>Selenium</li>
-              <li>Puppeteer</li>
-            </ul>
-            <h1>Base De Datos</h1>
-            <ul>
-            <li><h2>Relacionales</h2></li>
-            <ul>
-              <li>MySQL</li>
-              <li>SQLite</li>
-            </ul>
-            <li><h2>No Relacionales</h2></li>
-            <ul>
-              <li>MongoDB</li>
-            </ul>
-            </ul>
-            <h1>Herramientas de Desarrollo</h1>
-            <ul>
-              <li>Git</li>
-              <li>Github</li>
-              <li>Postman</li>
-              <li>Visual Studio Code</li>
-              <li>Eclipse</li>
-              <li>AWS S3</li>
-              <li>Docker</li>
-            </ul>
-            <h1>Sistemas Operativos</h1>
-            <ul>
-              <li>Windows</li>
-              <li>Linux</li>
-            </ul>
-            <h1>Herramientas de Gestion de Tickets</h1>
-            <ul>
-              <li>Monday</li>
-              <li>Jira</li>
-              <li>Trello</li>
-            </ul></p>
-          </div>
+          <SkillsSection />
 
         </div>
       <PageFooter />
