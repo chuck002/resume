0d3b581200ceae6928f472585e2ec29f7d7265de
chuck002 <ljavy86@gmail.com>
2024-05-13 22:59:08 -0300

    Add app.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './App.css'
 
 import {createHashRouter} from 'react-router-dom'
 import PageVersion from './version/PagesVersion/index.jsx'
