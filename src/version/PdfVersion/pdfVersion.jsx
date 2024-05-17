61a4396fc71141d97d0b377854f4d0ef9430d9f4
chuck002 <ljavy86@gmail.com>
2024-05-17 11:11:41 -0300

    Add json imports

--- src/version/PdfVersion/pdfVersion.jsx
+++ src/version/PdfVersion/pdfVersion.jsx
@@ -1,1 +1,8 @@
+import Home from '../../assets/Data/Home.json';
+import Experience from '../../assets/Data/Experience.json'
+import Skills from '../../assets/Data/Skills.json'
+import Education from '../../assets/Data/Education.json'
+import Projects from '../../assets/Data/Projects.json'
+
+
 export const htmlVersion = `<div><h1>holaa todso </h1></div>`
