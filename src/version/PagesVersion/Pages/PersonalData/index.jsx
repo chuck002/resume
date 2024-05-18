a95f4dd32377b7f9ec247f0810cf7fc2b047930f
chuck002 <ljavy86@gmail.com>
2024-05-18 10:52:54 -0300

    Add english version te PersonalData

--- src/version/PagesVersion/Pages/PersonalData/index.jsx
+++ src/version/PagesVersion/Pages/PersonalData/index.jsx
@@ -10,7 +10,12 @@ import PageFooter from '../../../../components/PageFooter';
 import { Typography, Card, CardContent } from '@mui/material';
 import HomeJson from '../../../../assets/Data/Home.json'
 
+import { LanguajeContext } from '../../../../context/LanguajeContext';
+import { useContext } from 'react';
+
 function PersonalData() {
+  const { languaje } = useContext(LanguajeContext);
+
   return (
     <>
       <div className='PageNavBarMovil'>
@@ -21,7 +26,7 @@ function PersonalData() {
       </div>
       <div className='Container-Title-Contact'>
         <Typography variant="h4" gutterBottom>
-          {HomeJson.spanish.contact.title}
+          {HomeJson[languaje].contact.title}
         </Typography>
 
 
@@ -29,19 +34,18 @@ function PersonalData() {
           <Card className='Container-Card-Index'>
             <CardContent>
 
-              <Avatar alt={HomeJson.spanish.image.alt} src={HomeJson.spanish.image.url} sx={{ width: 100, height: 100 }} />
-              <Typography variant="h5">Social Networks</Typography>
-              <GitHubIcon titleAccess='/chuck002' /><Link to='https://www.github.com/chuck002'>https://www.github.com/chuck002</Link>
-              <LinkedInIcon titleAccess='/javy-luna' /><Link to='https://www.linkedin.com/in/javy-luna'>https://www.linkedin.com/in/javy-luna</Link>
-              <AlternateEmailIcon titleAccess='Email' />
-              <p>Phone: +54 011 15 6264 5087</p>
-              <p>Linkedin: /javy-luna</p>
+              <Avatar alt={HomeJson[languaje].image.alt} src={HomeJson[languaje].image.url} sx={{ width: 100, height: 100 }} />
+              <Typography variant="h5">{HomeJson[languaje].contact.subtitle2}</Typography>
+              <GitHubIcon titleAccess='/chuck002' /><Link to={HomeJson[languaje].contact.github}>{HomeJson[languaje].contact.github}</Link>
+              <LinkedInIcon titleAccess='/javy-luna' /><Link to={HomeJson[languaje].contact.linkedin}>{HomeJson[languaje].contact.linkedin}</Link>
+              <p>Phone: {HomeJson[languaje].contact.phone}</p>
+              <p>Linkedin: {HomeJson[languaje].contact.linkedin}</p>
             </CardContent>
             <CardContent>
-              <Typography variant="h5">Personal Information</Typography>
+              <Typography variant="h5">{HomeJson[languaje].contact.subtitle1}</Typography>
               <p>Name: Javier Luna</p>
-              <p>Email: javylunadev@gmail.com</p>
-              <p>Phone: +54 011 15 6264 5087</p>
+              <p>Email: {HomeJson[languaje].contact.email}</p>
+              <p>Phone: {HomeJson[languaje].contact.phone}</p>
               <p>Linkedin: /javy-luna</p>
 
             </CardContent>
