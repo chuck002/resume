3601df07b11631c2ba592803927c68bd5dca235a
chuck002 <ljavy86@gmail.com>
2024-05-17 21:58:43 -0300

    Add english view for Education

--- src/version/PagesVersion/Pages/Education/index.jsx
+++ src/version/PagesVersion/Pages/Education/index.jsx
@@ -4,8 +4,11 @@ import PageFooter from '../../../../components/PageFooter';
 import { Card, CardContent, Typography, Tooltip, Chip } from '@mui/material';
 import EducationJson from '../../../../../src/assets/Data/Education.json';
 import './Education.css'
+import { useContext } from 'react';
+import { LanguajeContext } from '../../../../context/LanguajeContext';
 
 function Education() {
+  const { languaje } = useContext(LanguajeContext); 
   return (
     <>
       <div className='PageNavBarMovil'>
@@ -16,33 +19,33 @@ function Education() {
       </div>
       <div className='Container-Card-Education'>
         <Typography variant="h4" gutterBottom>
-          {EducationJson.languaje.spanish.Education.title}
+          {EducationJson.languaje[languaje].Education.title}
         </Typography>
         <div className='Container-Card-Content-Education'>
           <Card className='Container-Card-Index'>
             <CardContent className='Container-Card-Content-Index'>
               <Typography variant="h6" gutterBottom>
-                {EducationJson.languaje.spanish.Education.Formal.title}
+                {EducationJson.languaje[languaje].Education.Formal.title}
               </Typography>
               <div className='Lista-Experience-Title-Education'>
                 <Chip
                   size="medium"
-                  label={EducationJson.languaje.spanish.Education.Formal.data[0].Duration}
+                  label={EducationJson.languaje[languaje].Education.Formal.data[0].Duration}
                   color={'success'}
                   className='Lista-Experience-Title-Education'
                 />
-                <Typography component="span" className='Experience-empresa-Education'>{EducationJson.languaje.spanish.Education.Formal.data[0].Institution}</Typography>
+                <Typography component="span" className='Experience-empresa-Education'>{EducationJson.languaje[languaje].Education.Formal.data[0].Institution}</Typography>
               </div>
               <div className='Lista-Experience-Content-Education'>
-                <Typography component="span" className='Experience-empresa-Education'>{EducationJson.languaje.spanish.Education.Formal.data[0].Degree}</Typography>
-                <Typography component="span" className='Experience-empresa-Education'>{EducationJson.languaje.spanish.Education.Formal.data[0].Details}</Typography>
+                <Typography component="span" className='Experience-empresa-Education'>{EducationJson.languaje[languaje].Education.Formal.data[0].Degree}</Typography>
+                <Typography component="span" className='Experience-empresa-Education'>{EducationJson.languaje[languaje].Education.Formal.data[0].Details}</Typography>
               </div>
 
               <Typography variant="h6" gutterBottom>
-                {EducationJson.languaje.spanish.Education.Cursos.title}
+                {EducationJson.languaje[languaje].Education.Cursos.title}
               </Typography>
 
-              {EducationJson.languaje.spanish.Education.Cursos.data.map((item, index) => {
+              {EducationJson.languaje[languaje].Education.Cursos.data.map((item, index) => {
 
                 <span>{item.Duration}</span>
                 { console.log(item.Duration) }
